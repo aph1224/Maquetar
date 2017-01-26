b995ae81c11b1be4915e41537aeaa08eb632203b
Abraham Padilla <aphpadilla1224@gmail.com>
2017-01-26 15:58:56 -0600

    Primeros ejemplos
    Serie de ejercicios/ejemplos con el uso de CSS y JS...

---
+++ ModalBox/WithJS/Modal.js
@@ -0,0 +1,10 @@
+
+
+function openModal() {
+    document.getElementById('cover').style.display = "block";
+    document.getElementById('modalBox').style.height = "200px";
+}
+function closeModal() {
+    document.getElementById('cover').style.display = "none";
+    document.getElementById('modalBox').style.height = "0%";
+}
---
+++ ToggleHideShow/Toggle.js
@@ -0,0 +1,10 @@
+
+
+function toggle() {
+    var x = document.getElementById('toggle');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'block';
+    }
+}
